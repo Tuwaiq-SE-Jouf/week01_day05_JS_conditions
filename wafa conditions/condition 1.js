8db5d51b95674b37ede09d114c1846c05499e7c1
wafa-alruwaili <92299393+wafa-alruwaili@users.noreply.github.com>
2021-10-17 00:50:22 +0300

    Add files via upload

---
+++ wafa conditions/condition 1.js
@@ -0,0 +1,16 @@
+let number=prompt("Enter the grade");
+if(number>=90){
+    console.log("Grade is A");
+}
+else if(number>=80){
+    console.log("Grade is B");
+}
+else if(number>=70){
+    console.log("Grade is C");
+}else if(number>=60){
+    console.log("Grade is D");
+}
+else if(number<60){
+    console.log("Grade is F");
+}
+
---
+++ wafa conditions/condition 2.js
@@ -0,0 +1,10 @@
+let number=prompt("Enter the number");
+if (num % 2 == 0)
+{
+    console.log("The number even");
+}
+else
+{
+    console.log("The number odd");
+}
+
