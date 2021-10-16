19a29d1fa7d5eb42af07e21d68b5ac4f61f3446f
NaflaSale <naflasaleh@gmail.com>
2021-10-16 16:49:37 +0300

    HOMEWORK05

---
+++ grade.js
@@ -0,0 +1,17 @@
+//This is my js code
+let Grade = prompt ("ENTER YOUR GRADE :") ;
+if( Grade>=90){
+    alert("A");
+}
+else if(Grade >= 80){
+    alert("B");
+}
+else if(Grade >=70 ){
+    alert("C");
+}
+else if(Grade >=60 ){
+    alert("D");
+}
+else if(Grade <60 ){
+    alert("F");
+}
