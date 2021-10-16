1f8a46963d5d9531ae1aaacb6dad9c8b0a91631b
asmaalnaser <asmahn1996@gmail.com>
2021-10-16 16:44:05 +0300

    homework

---
+++ 1996.js
@@ -0,0 +1,4 @@
+let year = prompt("Enter year")
+            if ((year%4==0)&&(year!=100)||(year%400==0)){console.log("is Leap year");}
+            else {console.log("is Not Leap year");
+            }
