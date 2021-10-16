a2f25b1fba0d773615203120ff08be38535c0356
Kh-112956 <92294786+Kh-112956@users.noreply.github.com>
2021-10-17 01:44:00 +0300

    Add files via upload

---
+++ k.js
@@ -0,0 +1,17 @@
+
+let k = prompt ("ENTER YOUR GRADE :") ;
+if( k>=90){
+    alert("A");
+}
+else if(k>= 80){
+    alert("B");
+}
+else if(k >=70 ){
+    alert("C");
+}
+else if(k >=60 ){
+    alert("D");
+}
+else if(k<60 ){
+    alert("F");
+}
