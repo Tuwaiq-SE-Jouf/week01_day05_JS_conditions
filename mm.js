7ff96d582c4d787293989ee9843636fb61f493ca
Meshal552 <meshl5541@gmail.com>
2021-10-16 10:47:44 +0300

    homework

---
+++ mm.js
@@ -0,0 +1,8 @@
+let number = prompt(" Enter the Grade")
+if (number >= 90) { console.log("the Grade is A"); }
+else if (number >= 80 || number == 89) {
+    console.log("the Grade is B");
+}
+else if (number >= 70 || number == 79) { console.log("the Grade is C") }
+else if (number >= 60 || number == 69) { console.log("the Grade is D"); }
+else { console.log("the Grade is F"); }
