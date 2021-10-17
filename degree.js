409e93cc5f3c4e43ffc3a61c408c50a24fa16270
engtalal2022 <88284863+EngTalal2022@users.noreply.github.com>
2021-10-17 09:18:29 +0300

    Add files via upload

---
+++ degree.js
@@ -0,0 +1,18 @@
+let number=prompt("please inter degree");
+if (number>=90){
+    console.log("A");
+}
+else if (number>=80){
+    console.log("B");
+}
+    
+else if (number>=70){
+    console.log("C");
+} 
+else if  (number>=60){
+    console.log("D");
+}
+else if (number<60) {
+
+    console.log("F")
+}
