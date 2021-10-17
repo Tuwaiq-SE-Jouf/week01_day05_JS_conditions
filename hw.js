c9d3ec543ed004444ac80ddd23690be97153e0d8
HayaLafi <92294695+HayaLafi@users.noreply.github.com>
2021-10-17 09:55:53 +0300

    chang the fil and upload

---
+++ hw.js
@@ -0,0 +1,17 @@
+let hw = prompt (ENTER YOUR GRADE :");
+if (hw>=90){
+    alert("A")
+}
+else if (hw>= 80){
+    alert ("B");
+
+}
+else if (hw>= 70){
+    alert ("C");
+}
+    else if (hw>= 60){
+        alert ("D");
+    }
+        else if (h<60){
+            alert ("f");
+        }
---
+++ v.js
@@ -0,0 +1,9 @@
+const number = prompot ("Enter a number: ")
+// check if the number is even 
+if (number %2 == 0){
+    alert(" the number is enen.")
+}
+//if the number is odd 
+else {
+    alert("the number is onloadeddata.")
+}
