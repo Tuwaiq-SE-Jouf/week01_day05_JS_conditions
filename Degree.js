9b20d8b864039abe9c0c860ecde9d62c5122e003
MISSARWAFAHAD1 <92294869+MISSARWAFAHAD1@users.noreply.github.com>
2021-10-29 13:08:56 +0300

    Add files via upload

---
+++ Degree.js
@@ -0,0 +1,19 @@
+let number = prompt(" Enter the Grade")
+
+if (number >= 90) { 
+    console.log(" Grade is A");
+ }
+ 
+  else if (number >= 80 || number == 89) {
+    console.log(" Grade is B");
+}
+else if (number >= 70 || number == 79) {
+     console.log(" Grade is C");
+     }
+     else if (number >= 60 || number == 69) { 
+        console.log(" Grade is D");
+     }
+     
+else 
+{ console.log(" Grade is F");
+ }
---
+++ year.js
@@ -0,0 +1,10 @@
+//year leap or not leap 
+let year = prompt("Enter year")
+if(year % 400 == 0){console.log("is Leap year");}
+else if(year%100 ==0){
+    console.log("is Leap year");}
+    else if(year%4 ==0){
+        console.log("is Leap year");}
+        else {
+            console.log("is Not Leap year");}
+            
